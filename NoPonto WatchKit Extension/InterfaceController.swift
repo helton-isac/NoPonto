05ca31ce1e965110d9d994d1b91ba15fe239955a
Helton Isac <helton.isac@gmail.com>
2020-11-03 21:19:02 -0300

    Initial Commit - Story Board and Outlets

--- NoPonto WatchKit Extension/InterfaceController.swift
+++ NoPonto WatchKit Extension/InterfaceController.swift
@@ -11,16 +11,56 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var timer: WKInterfaceTimer!
+    @IBOutlet weak var buttonTimer: WKInterfaceButton!
+    @IBOutlet weak var labelWeight: WKInterfaceLabel!
+    @IBOutlet weak var groupText: WKInterfaceGroup!
+    @IBOutlet weak var groupImage: WKInterfaceGroup!
+    @IBOutlet weak var labelCook: WKInterfaceLabel!
+    @IBOutlet weak var sliderCook: WKInterfaceSlider!
+    @IBOutlet weak var labelTemperature: WKInterfaceLabel!
+    @IBOutlet weak var pickerWeight: WKInterfacePicker!
+    @IBOutlet weak var pickerTemperature: WKInterfacePicker!
+    
+    
     override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
         // Configure interface objects here.
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
     }
-
+    
+    // MARK: - IBActions
+    @IBAction func minus() {
+    }
+    
+    @IBAction func plus() {
+    }
+    
+    @IBAction func toogleTimer() {
+    }
+    
+    @IBAction func onSliderChange(_ value: Float) {
+    }
+    
+    @IBAction func onWeightPickerChange(_ value: Int) {
+    }
+    
+    
+    @IBAction func onTemperaturePickerChange(_ value: Int) {
+    }
+    
+    @IBAction func onModeChange(_ value: Bool) {
+    }
+    
 }
